894c35f7729f22fd6f130ac0b6ed0890d9fee084
lavaaash <vital229008@Mail.ru>
2025-03-05 23:41:52 +0700

    update backend

--- backend/app.py
+++ backend/app.py
@@ -15,21 +15,21 @@ conn = psycopg2.connect(
     )
 cur = conn.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, name TEXT, surname TEXT, role TEXT, party TEXT);")
+cur.execute("CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, name TEXT, surname TEXT, patronymic TEXT, role TEXT, party TEXT);")
 conn.commit()
-cur.execute("CREATE TABLE IF NOT EXISTS tasks (task_id SERIAL PRIMARY KEY, task TEXT, subject TEXT, date DATE, party TEXT);")
+cur.execute("CREATE TABLE IF NOT EXISTS tasks (task_id SERIAL PRIMARY KEY, creator TEXT, task TEXT, subject TEXT, deadline DATE, party TEXT, date DATE);")
 conn.commit()
 
-# Пример маршрута
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    return jsonify({"message": "Hello from Flask!"})
+# # Пример маршрута
+# @app.route('/api/hello', methods=['GET'])
+# def hello():
+#     return jsonify({"message": "Hello from Flask!"})
 
 # Пример POST-запроса
-@app.route('/api/data', methods=['POST'])
-def receive_data():
-    data = request.json  # Получаем JSON-данные
-    return jsonify({"received": data})
+# @app.route('/api/data', methods=['POST'])
+# def receive_data():
+#     data = request.json  # Получаем JSON-данные
+#     return jsonify({"received": data})
 
 @app.get("/api/homeWorkGet")
 def get_homework():
@@ -65,72 +65,119 @@ def get_homework():
         return jsonify({"error": str(e)}), 500
 
 
-@app.post("/api/adduser")
+@app.post("/api/user")
 def adduser():
     data = request.get_json()
-    name = data["name"]
-    surname = data["surname"]
-    role = data["role"]
-    party = data["party"]
+    if not data:
+        return jsonify({"error": "data is required"}), 400  # Проверяем, что данные не переданы
 
-    cur.execute("""
-                INSERT INTO users (name, surname, role, party)
-                VALUES (%s, %s, %s, %s) RETURNING user_id;
-                """, (name, surname, role, party))
-    conn.commit()
+    try:
+        name = data["name"]
+        surname = data["surname"]
+        patronymic = data["patronymic"]
+        role = data["role"]
+        party = data["party"]
 
-    user_id = cur.fetchone()[0]
-    return {"id": user_id, "message": f"User {name} {surname} registered."}, 201
+        cur.execute("""
+                    INSERT INTO users (name, surname, patronymic, role, party)
+                    VALUES (%s, %s, %s, %s, %s) RETURNING user_id;
+                    """, (name, surname, patronymic, role, party))
+        conn.commit()
+
+        user_id = cur.fetchone()[0]
+        return jsonify({"id": user_id, "message": f"User {name} {surname} {patronymic} registered."}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.post("/api/task")
 def settask():
     data = request.get_json()
-    task = data["task"]
-    subject = data["subject"]
-    date = data["date"]
-    party = data["party"]
+    if not data:
+        return jsonify({"error": "data is required"}), 400  # Проверяем, что данные не переданы
 
-    cur.execute("""
-                INSERT INTO tasks (task, subject, date, party)
-                VALUES (%s, %s, %s, %s) RETURNING task_id;
-                """, (task, subject, date, party))
-    conn.commit()
+    try:
+        task = data["task"]
+        subject = data["subject"]
+        date = data["date"]
+        party = data["party"]
+        
+        cur.execute("""
+                    INSERT INTO tasks (task, subject, date, party)
+                    VALUES (%s, %s, %s, %s) RETURNING task_id;
+                    """, (task, subject, date, party))
+        conn.commit()
 
-    task_id = cur.fetchone()[0]
-    return {"id": task_id, "message": f"Task for {subject} created"}, 201
-
-@app.get("/api/task/<party>")
-def gettask(party):
-    cur.execute("""
-                SELECT * FROM tasks
-                WHERE party = %s;""", (party,))
-    tasks = cur.fetchall()
-    return tasks
+        task_id = cur.fetchone()[0]
+        return jsonify({"id": task_id, "message": f"Task for {subject} created"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.put("/api/task/<task_id>")
 def updatetask(task_id):
     data = request.get_json()
-    task = data["task"]
-    cur.execute("""
-                UPDATE tasks
-                SET task = %s
-                WHERE task_id = %s
-                RETURNING task_id;
-                """, (task, task_id))
-    conn.commit()
-    task_id = cur.fetchone()[0]
-    return {"id": task_id, "message": f"Task {task_id} updated"}, 200
+    if not data:
+        return jsonify({"error": "data is required"}), 400  # Проверяем, что данные не переданы
+
+    try:
+        task = data["task"]
+        cur.execute("""
+                    UPDATE tasks
+                    SET task = %s
+                    WHERE task_id = %s
+                    RETURNING task_id;
+                    """, (task, task_id))
+        conn.commit()
+        task_id = cur.fetchone()[0]
+        return {"id": task_id, "message": f"Task {task_id} updated"}, 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 @app.delete("/api/task")
 def deletetask():
-    data = request.get_json() #fdsaf
-    task_id = data["task_id"]
-    cur.execute("""
-                DELETE FROM tasks
-                WHERE task_id = %s
-                """, (task_id,))
-    conn.commit()
-    return {"id": task_id, "message": f"Task {task_id} deleted"}, 200
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "data is required"}), 400  # Проверяем, что данные не переданы
+
+    try:
+        task_id = data["task_id"]
+        cur.execute("""
+                    DELETE FROM tasks
+                    WHERE task_id = %s
+                    """, (task_id,))
+        conn.commit()
+        return {"id": task_id, "message": f"Task {task_id} deleted"}, 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.get("/api/user")
+def getuser():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "data is required"}), 400  # Проверяем, что данные не переданы
+    try: 
+        user_id = data["user_id"]
+        cur.execute("""
+            SELECT name, surname, patronymic, party
+            FROM users
+            WHERE user_id = %s;
+        """, (user_id,))
+
+        results = cur.fetchall()  # Получаем все строки
+
+        if results:
+            info = [
+                {
+                    "name": row[0],
+                    "surname": row[1],
+                    "patronymic": row[2],
+                    "party": row[3]
+                }
+                for row in results
+            ]
+            return jsonify(info)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- src/ip.js
+++ src/ip.js
@@ -1,1 +1,1 @@
-export const ip = "192.168.0.137";
+export const ip = "192.168.0.16";
